c5d11b8c03093cc362fa6c82ab7872079af0b19a
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-11-06 18:10:14 -0800

    group data and bar chart with correlation heatmap

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ data = pd.read_excel("mydata.xlsx")
 data.Prices.hist()
 plt.show()
 
-grouped_data = data.groupby(by="Categories").mean()
-plt.bar(grouped_data.index, grouped_data.Price)
+grouped_data = data.groupby(by="Categories").mean(numeric_only=True)
+plt.bar(grouped_data.index, grouped_data["Prices"])
+plt.show()
+
+one_hot_categories = pd.get_dummies(data.Categories)
+data = data.join(one_hot_categories)
+data.drop(["Categories"], axis = 1)
+
+sns.heatmap(data.corr(), annot=True, cmap="YLGnBu")
 plt.show()
